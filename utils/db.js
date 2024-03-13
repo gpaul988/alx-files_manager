391160bb4413ef563d2d7b72c29d38229ee50feb
gpaul988 <gpaul988gmail.com>
2024-03-13 14:04:59 +0100

    fix

--- utils/db.js
+++ utils/db.js
@@ -4,17 +4,17 @@ import Collection from 'mongodb/lib/collection';
 import envLoader from './env_loader';
 
 /**
- * Represents a MongoDB client.
+ * Shows a MongoDB client.
  */
 class DBClient {
   /**
-   * Creates a new DBClient instance.
+   * Generate a new DBClient instance.
    */
   constructor() {
     envLoader();
+    const database = process.env.DB_DATABASE || 'files_manager';
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
     const dbURL = `mongodb://${host}:${port}/${database}`;
 
     this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
