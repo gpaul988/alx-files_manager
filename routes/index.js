09801159c336608bc00b78dd9ad250c6d33bec74
gpaul988 <gpaul988gmail.com>
2024-03-13 18:02:56 +0100

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
-import { Express } from 'express';
 import AppController from '../controllers/AppController';
+import { Express } from 'express';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 import FilesController from '../controllers/FilesController';
@@ -8,7 +8,7 @@ import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 import { APIError, errorResponse } from '../middlewares/error';
 
 /**
- * Injects routes with their handlers to the given Express application.
+ * Inputs routes and their handlers to the given Express application.
  * @param {Express} api
  */
 const injectRoutes = (api) => {
